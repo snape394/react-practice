cb6de431376b6c42b5ae912dd45a77542440cf5e
Ojas Agrawal <ojasagrawal1996@gmail.com>
2021-02-22 23:22:29 +0530

    commit

--- src/components/second-component.tsx
+++ src/components/second-component.tsx
@@ -1,17 +1,30 @@
 
+import { makeStyles } from '@material-ui/core';
+import Chip from '@material-ui/core/Chip';
 import React, { useEffect, useState } from 'react'
 import { from, Observable, Subscription } from 'rxjs'
 import { TypedJSON } from 'typedjson'
 import {UserDetail} from './responses/user-detail.response'
 
 
-
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+      '& > *': {
+        margin: theme.spacing(0.5),
+      },
+    },
+  }));
 
 
 export const SecondComponent:React.FC = () => {
 
+    const styles = useStyles()
     const [userData, setUserData] = useState<Subscription>()
-    const [userDetail, setuserDetail] = useState<UserDetail>()
+    const [userDetail, setUserDetail] = useState<UserDetail[]>()
+    const [isLoading, setisLoading] = useState(true);
     const serializer = new TypedJSON(UserDetail);
     
 
@@ -23,10 +36,11 @@ export const SecondComponent:React.FC = () => {
 
     const getData = () =>
     {
-        setUserData(apiCall().subscribe( response=>{response.forEach((e:any) => {
-            const a = serializer.parse(e)
-            console.log(a)
-        });}));
+        setUserData(apiCall().subscribe( response=>{
+                setUserDetail(response)}
+
+        ))
+           
     }
 
 
@@ -42,14 +56,21 @@ export const SecondComponent:React.FC = () => {
 
     return (
         <div>
-            <h1>Second Component</h1>
-            <button onClick={getData}>Subscribe!</button>
-            {userDetail}
+            <div>
             
+            <h1>Second Component</h1>
+            <button onClick={getData}>Subscribe!</button></div>
+            {userDetail?.map((response:UserDetail)=>
+            <div key={response.id}  className={styles.root}>
+                <Chip  label={`${response.id}----${response.name}`} />
 
+            </div>
+ 
+    )}
         
        
      
         </div>
-    )
+        )
+    
 }
